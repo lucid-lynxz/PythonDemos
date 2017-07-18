539211cee7397f4dbf5200a46aa311013b39e6a5
lynxz <lynxz8866@gmail.com>
2017-07-18 11:25:07 +0800

    openpyxl区域选择

--- office/openpyxl_demo.py
+++ office/openpyxl_demo.py
@@ -3,6 +3,8 @@
 
 '''
 利用openpyxl操作excel
+文档地址: http://openpyxl.readthedocs.io/en/default/#
+tutorial地址: http://www.pythonexcel.com/openpyxl.php
 测试后发现,不支持openoffice格式(.ods),只支持 .xlsx
 '''
 
@@ -17,9 +19,9 @@ wb = openpyxl.load_workbook(src_excel_path)
 print(type(wb))
 
 # 创建新工作簿
-newWb = openpyxl.Workbook()
-newWb.create_sheet("hello openpyxl", 0)
-newWb.save("py_src_new.xlsx")
+# newWb = openpyxl.Workbook()
+# newWb.create_sheet("hello openpyxl", 0)
+# newWb.save("py_src_new.xlsx")
 
 try:
     # 创建工作表
@@ -46,6 +48,8 @@ try:
     # 获取有数据的区域最大行号列号: sum(1 for _ in sheet1.columns)  sum(1 for _ in sheet1.rows)
     print("sheet1 相关信息 ", type(sheet1), sheet1.title, sum(1 for _ in sheet1.columns), sum(1 for _ in sheet1.rows))
     print("max_column", sheet1.max_column, sheet1.max_row, len(sheet1['A']))
+    colA = tuple(sheet1.columns)[:1]
+    print("col length: ", len(colA))
 
     # 获取指定单元格对象
     cell = sheet1['a1']
@@ -67,14 +71,22 @@ try:
     area = tuple(sheet1['a1':'c3'])
     print("length = ", len(area))
 
-    # 从当前区域的下一行开始,插入40行,每行添加0~99共计100个单元格数据
-    for row in range(40):
-        sheet1.append(range(9))
+    # 选择第1到9行的区域
+    for x in range(1, 10):
+        print(x, sheet1.cell(row=x, column=1).value)
 
+    # 从当前区域的下一行开始,插入10行,每行添加0~9共计10个单元格数据
+    for row in range(10):
+        sheet1.append(range(10))
 
+    # 在当前有效区域的下一行追加一行数据,需要传入tuple,list 或 generator , 从A1单元格开始填充
+    sheet1.append(["append operation", "by openpyxl"])
+    wb.save(src_excel_path)
 
 except KeyError as e:
     print("error ", e)
+# except ValueError as e:
+#     print("ValueError ", e)
 
 # 列字母和数字之间的转换
 print(openpyxl.utils.get_column_letter(1), openpyxl.utils.column_index_from_string('aa'))
