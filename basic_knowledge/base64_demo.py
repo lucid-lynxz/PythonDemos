ff7176d87ccb66c6a5373a1a459a3c8878208a21
lynxz <lynxz8866@gmail.com>
2017-08-02 15:31:51 +0800

    指定编码

--- basic_knowledge/base64_demo.py
+++ basic_knowledge/base64_demo.py
@@ -14,3 +14,7 @@ print('encode result is: %s' % result)
 
 decode_result = base64.b64decode(result)
 print(str(decode_result, 'utf8'))
+
+src = 'u7bTrcq508PM2tG2xvPStdPKz+Q='
+decode_result = base64.b64decode(src)
+print(str(decode_result, 'gb2312'))
