970dcd9896f016c7f59579fb3173fa06f53432b3
lynxz <lynxz8866@gmail.com>
2017-08-02 17:15:21 +0800

    正则finall demo

--- basic_knowledge/re_demo.py
+++ basic_knowledge/re_demo.py
@@ -1,6 +1,7 @@
 import re
 
 # 判断是否匹配,若匹配则返回 Match 对象,否则返回 None
+# match从开头开始匹配
 obj = re.match(r'^\d{3}-\d{3,8}$', '010-12345')
 if obj:
     print("匹配成功")
@@ -26,11 +27,18 @@ print(m.group(0))  # 返回原始字符串
 print(m.group(1))  # 返回第1个子串
 print(m.group(2))  # 返回第2个子串
 
-# match判定字符串是否符合给定的规则
-# search可以提取按给定规则提取子串
-s = '"=?gb2312?B?zNrRtsbz0rXTys/k?="'
+# match判定字符串是否符合给定的规则,从头开始判定
+# search可以提取按给定规则提取子串,匹配第一次
+# finall 匹配所有满足条件的子串
+s = '=?UTF-8?Q?Re:_supersonic-android_|_=E5=8D=87=E7=BA=A7oifi-sdk?=\r\n=?UTF-8?Q?_1.8.1_=28#30=29?='
 match = re.search(r'=\?.*\?=', s)
 print(s, "   搜索子串结果:   ", match)
+all_sub = re.findall(r'=\?.*\?=', s)
+print("匹配所有子串结果: ", len(all_sub), type(all_sub))
+
+for index in range(len(all_sub)):
+    print("   子串 ==> ",all_sub[index])
+
 if match:
     result = match.group()
     print('获取子串成功: ', result)
