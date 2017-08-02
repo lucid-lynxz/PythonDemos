4cba78693f1a72288641ff6211c9aa7b8fba6745
lynxz <lynxz8866@gmail.com>
2017-08-02 17:42:13 +0800

    解析邮件内容方法优化

--- utils/auto_send_email.py
+++ utils/auto_send_email.py
@@ -7,6 +7,7 @@
 
 from email.mime.text import MIMEText
 from email.header import Header
+from email.header import decode_header
 from smtplib import SMTP_SSL
 import email, imaplib, base64, re
 
@@ -14,11 +15,11 @@ import email, imaplib, base64, re
 smtp_server = 'smtp.exmail.qq.com'
 imap_server = 'imap.exmail.qq.com'
 # sender_qq为发件人的qq号码
-email_account = '55**@qq.com'
+email_account = '44***@qq.cn'
 # pwd为邮箱密码或者授权码
-pwd = '***'
+pwd = 'lynx***'
 # 收件人邮箱
-receiver = '44**@qq.com'
+receiver = '55***@qq.com'
 
 # 邮件主体
 mail_subject = 'python测试'
@@ -28,35 +29,61 @@ mail_content = 'python登录qq邮箱发邮件测试'
 
 # 对收取的邮件内容进行解码显示,源数据类似: '"=?gb2312?B?zNrRtsbz0rXTys/k?="   <10000@qq.com>'
 def decode_mail_info(info):
-    detail = re.split(r'\s+', info)
+    # 可能报错: TypeError: expected string or bytes-like object ,比如 撤回邮件时
+    # print("==> ", type(info), isinstance(info, (str, bytes)), info)
+    if not isinstance(info, (str, bytes)):
+        return info
+
+    detail = re.split(r'"\s+', info)
     if len(detail) >= 2:
         detail = detail[1]
     else:
         detail = ""
-
-    match = re.search(r'=\?.*\?=', info)
-    if match:
-        result = match.group()
-        data = result.split(sep='?')
-        if len(data) >= 5:
-            src_coding = data[1]
-            cur_coding = data[2]
-            value = data[3]
-            decode_result = base64.b64decode(value)
-            actualValue = str(decode_result, str(src_coding)) + detail
-            print("ori: %s\tnow: %s" % (info, actualValue))
-            return actualValue
-    else:
-        print("未找到有效信息 %s " % info)
+    all_result = re.findall(r'=\?.*\?=', info)
+    result_info = ''
+    for index in range(len(all_result)):
+        part = all_result[index]
+        text, encoding = decode_header(part)[0]
+        actualValue = str(text, encoding=str(encoding))
+        result_info += actualValue
+    result_info += detail
+    if len(result_info) == 0:
+        result_info = info
+    print("result_info = ", result_info)
+    return result_info
+    # match = re.search(r'=\?.*\?=', info)
+    # if match:
+    #     result = match.group()
+    #     text, encoding = decode_header(result)[0]
+    #     actualValue = str(text, encoding=str(encoding))
+    #     print("==>", actualValue)
+    #     # print("%s -----> %s \n %s" % (text, encoding, actualValue))
+    #
+    #     # data = result.split(sep='?')
+    #     # if len(data) >= 5:
+    #     #     src_coding = data[1]
+    #     #     cur_coding = data[2]
+    #     #     value = data[3]  # =?UTF-8?Q?***?=
+    #     #     decode_result = base64.b64decode(value)
+    #     #     print("src_coding is : %s , %s , %s , %s" % (src_coding, str(src_coding), decode_result, value))
+    #     #     # actualValue = str(decode_result, encoding=str(src_coding)) + detail
+    #     #     actualValue = str(b'\xe5\x8d\x87\xe7\xba\xa7', encoding=str(src_coding)) + detail
+    #     #     print("ori: %s\tnow: %s" % (info, actualValue))
+    #     #     return actualValue
+    #     # else:
+    #     #     return info
+    # else:
+    #     print("未找到有效信息 %s " % info)
+    #     return info
 
 
 # 使用指定sender邮箱账号密码,登录到 smtp_server 服务器(smtp服务器,使用ssl),发送邮件给指定receiver
-def send_mail_by_smtp_ssl(sender_mail_account='from@gmail.com', sender_mail_pwd='',
-                          receiver_mail_account='lucid_lynxz@gmail.com',
-                          smtp_server='', ssl_port=465, subject='from python', content='hello'):
+def send_mail_by_smtp_ssl(sender_mail_account='', sender_mail_pwd='',
+                          receiver_mail_account='',
+                          smtp_server='', ssl_port=465, subject='', content=''):
     # ssl登录
     smtp = SMTP_SSL(smtp_server, port=ssl_port)
-    smtp.set_debuglevel(1)  # 1-开启调试 0-关闭
+    # smtp.set_debuglevel(1)  # 1-开启调试 0-关闭
     smtp.ehlo(smtp_server)
     smtp.login(sender_mail_account, sender_mail_pwd)
 
@@ -73,9 +100,25 @@ def get_mail_by_imap_ssl(email_account='', email_pwd='', imap_server='', ssl_por
     conn = imaplib.IMAP4_SSL(host=imap_server, port=ssl_port)  # 如果未启用ssl,使用 imaplib.IMAP4(...)
     conn.login(email_account, email_pwd)
     conn.select()  # 选择指定的文件夹,默认为 INBOX , 按时间先后顺序读取
+    '''
+    search() 搜索指定类型的邮件
+    参考: http://blog.csdn.net/longzhiwen888/article/details/46562723
+    ALL - 所有邮件
+    Recent - 未读邮件,我测试了下貌似无效
+    Seen - 已读邮件
+    Answered - 已回复的邮件
+    '''
     type, data = conn.search(None, 'ALL')  # 搜索匹配目录下的邮件
+    # for num in data[0].split():
+    #     email_type, email_data = conn.fetch(num, '(RFC822)')
+    #     print('Message %s\n%s\n' % (num, email_data[0][1]))
     newList = data[0].split()
-    type, data = conn.fetch(newList[0], '(RFC822)')
+    mail_count = len(newList)
+    if mail_count == 0:
+        print("未搜索到符合条件的邮件")
+        return
+    # print("总邮件数量: ", mail_count)
+    type, data = conn.fetch(newList[mail_count - 1], '(RFC822)')  # 读取最新的邮件信息
     msg = email.message_from_bytes(data[0][1])
     '''
     b'Date: Mon, 12 Dec 2016 17:41:30 +0800\r\n
@@ -87,10 +130,11 @@ def get_mail_by_imap_ssl(email_account='', email_pwd='', imap_server='', ssl_por
     charset="gb2312"\n\n
     <!DOCTYPE html*****n'
     '''
-    print(data[0][1])
-    sender = decode_mail_info(msg.get("From"))
-    receiver = decode_mail_info(msg.get("To"))
-    subject = decode_mail_info(msg.get('subject'))
+    # print(data[0][1])
+    sender = decode_mail_info(msg.get("From"))  # 发件人
+    receiver = decode_mail_info(msg.get("To"))  # 收件人
+    subject = decode_mail_info(msg.get('subject'))  # 邮件主题
+    date = decode_mail_info(msg.get('date'))  # 邮件时间
     conn.close()
     conn.logout()
     # msg.get('subject') 得到的邮件主体内容: =?gb2312?B?u7bTrcq508PM2tG2xvPStdPKz+Q=?=
@@ -102,10 +146,6 @@ def get_mail_by_imap_ssl(email_account='', email_pwd='', imap_server='', ssl_por
     # 解码方法:
     # echo u7bTrcq508PM2tG2xvPStdPKz+Q= | base64 -D | iconv -f gbk -t utf-8
 
-    # for num in data[0].split():
-    #     email_type, email_data = conn.fetch(num, '(RFC822)')
-    #     print('Message %s\n%s\n' % (num, email_data[0][1]))
 
-
-send_mail_by_smtp_ssl(email_account, pwd, receiver, smtp_server, 465, mail_subject, mail_content)
+# send_mail_by_smtp_ssl(email_account, pwd, receiver, smtp_server, 465, mail_subject, mail_content)
 get_mail_by_imap_ssl(email_account, pwd, imap_server, 993)
